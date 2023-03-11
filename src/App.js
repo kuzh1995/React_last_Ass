d07a67046fc22834b45ce12248620c0cc291fdbd
kuzh1995 <117057320+kuzh1995@users.noreply.github.com>
2023-03-11 22:49:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Routes from "./components/routes/Routes";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+      <Header/>
+        <div>
+          <Routes/>
+        </div>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
